7a500a0dc8dfc8939271b7f77a86bf98b7861e49
Bhupendra Aole <baole@imminet.com>
2018-02-09 10:23:42 -0500

    command line exec()

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ with open('data.txt') as f:
 
 default_team = None
     
-def cmd_teams(arg):
+def all_teams(arg):
     idx=0
     for team in teams:
         idx += 1
@@ -27,40 +27,51 @@ def cmd_tournament(arg):
     t = Tournament(data, default_team=default_team)
     t.play()
     
-def cmd_help(arg):
-    print('Commands..')
-    for k in commands.keys():
-        print(k)
-        
-def cmd_create(arg):
+def create_team( team ):
     '''
     Create a team.
     11 random players will be added.
     this will become the default team of the Coach.
     '''
-    tname = arg.split()[1]
-    teams.append(tname)
-    default_team = tname
+    global default_team
     
-    print('Team', tname+':')
+    teams.append(team)
+    default_team = team
+    
     players=[]
     for i in range(11):
         name = names.get_full_name(gender='male')
         players.append(name)
-        print(name)
-    data[tname]=players
+    data[team]=players
     
-commands = { 'teams':cmd_teams, 'match':cmd_match, 'tournament':cmd_tournament, 'help':cmd_help, 'create':cmd_create }
-
+    print_team( team )
+    
+def add_player( pos, name, team=None ):
+    '''
+    add new player to the default team at a specific position
+    '''
+    if not team:
+        team = default_team
+        
+    data[team].insert(pos-1, name)
+    
+    print_team( team )
+    
+def print_team(team=None):
+    if not team:
+        team = default_team
+        
+    print('Team', team+':')
+    cnt=1
+    for p in data[team]:
+        print(str(cnt)+'. '+p)
+        cnt+=1
+    
 while True:
     print('cmd>',end=' ')
     cmd = input()
     if cmd=='quit' or cmd=='exit':
         break
     
-    func = cmd.split(' ',1)[0]
-    if func not in commands:
-        print('Command \''+func+'\' '+'not found! Type \'help\' to get list of commands.')
-    else:
-        commands[func](cmd)
-        
+    exec( cmd, globals() )
+    
